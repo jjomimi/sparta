b523919ddea7d993d4c92b0b263a61de6397f551
jjomimi <67014929+jjomimi@users.noreply.github.com>
2020-09-02 21:07:16 +0900

    최종코드

--- my_project/crawl/vegan_restaurant.py
+++ my_project/crawl/vegan_restaurant.py
@@ -12,23 +12,22 @@ options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
 options.add_argument("disable-gpu")
-options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
 
-
-driver = webdriver.Chrome('C:/Users/love8/OneDrive/바탕 화면/sparta/chromedriver_win32/chromedriver.exe',options=options)
+driver = webdriver.Chrome('C:/Users/love8/OneDrive/바탕 화면/sparta/chromedriver_win32/chromedriver.exe', options=options)
 url = 'https://map.kakao.com/?map_type=TYPE_MAP&q=%EB%B9%84%EA%B1%B4+%EB%A0%88%EC%8A%A4%ED%86%A0%EB%9E%91&urlLevel=10&urlX=621710&urlY=1098130'
 driver.get(url)
 
 driver.find_element_by_id('search-input').send_keys('비건 식당')
 driver.find_element_by_css_selector('#search\.keyword\.submit').click()
 
+page_bar = driver.find_element_by_css_selector("#info\.search\.page\.no2")
+page_bar[0].click()
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
 items = list(soup.select('#info\.search\.place\.list > li'))
-
-for n in range(1, 20):
-    time.sleep(2)
 
 for item in items:
     validate_tag = item.select_one('div.head_item.clickArea > strong > a.link_name')
@@ -40,19 +39,6 @@ for item in items:
         phone = item.select_one('div.contact.clickArea > span.phone').text.strip()
         print(name, star, address, openhour, phone)
 
-        # page_bar = driver.find_element_by_css_selector("div.seach.pget")
-        # page_bar[2].click()
-        #
-        #
-        # try:
-        #     if n % 5 != 0:
-        #         page_bar[n % 5 + 1].click()
-        #     else:
-        #         page_bar[6].click()
-        # except:
-        #     print("수집완료")
-        #     break
-
         doc = {
             'name': name,
             'star': star,
@@ -60,13 +46,6 @@ for item in items:
             'openhour': openhour,
             'phone': phone
 
-
-}
+        }
         db.vegan.insert_one(doc)
-
-
-
-
-
-
 
